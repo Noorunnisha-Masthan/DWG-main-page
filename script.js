fe2c6afa0c161967030dd70db90c974574219383
Noorunnisha-Masthan <noorunnishamasthan@gmail.com>
2023-10-06 23:48:45 +0530

    noor

---
+++ script.js
@@ -0,0 +1,9 @@
+const wrapper = document.querySelector('.wrapper');
+const loginlink = document.querySelector('.login-link');
+
+const btnpopup = document.querySelector('.btnlogin-popup');
+
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup')
+});
